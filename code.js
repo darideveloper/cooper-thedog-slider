be8bbd0f6022767aef436360adcf98c66b45edec
daridev <darideveloper@gmail.com>
2024-07-15 16:21:20 -0600

    feat (slider): auto generate slides

--- code.js
+++ code.js
@@ -1,6 +1,20 @@
+// Create slides
+const slider = document.querySelector('#slider .splide__list')
+for (slideData of slidesData) {
+
+  const slideElem = `
+    <li class="splide__slide">
+      <a href="${slideData.link}">
+          <img src="${slideData.src}" alt="${slideData.alt}" />
+      </a>
+    </li>
+  `
+  slider.innerHTML += slideElem
+}
+
+// Initialize the slider
 var splide = new Splide('.splide', {
   type: 'loop',
   padding: '10%',
 })
-
 splide.mount()
