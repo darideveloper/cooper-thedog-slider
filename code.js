b853e6a00602bbfa83428c37f9920b99ab213d4c
daridev <darideveloper@gmail.com>
2024-07-16 13:52:13 -0600

    feat (buttons): render items

--- code.js
+++ code.js
@@ -19,47 +19,95 @@ var splide = new Splide('.splide', {
 })
 splide.mount()
 
-// Open submenu
+
+// Menu global elems
 let currentSubmenu = ""
 const submenuElem = document.querySelector('.submenu-wrapper')
-const menuButtons = document.querySelectorAll('.menu > li')
+const menuButtonsElems = document.querySelectorAll('.menu > li')
 const menuElem = document.querySelector('.menu')
-for (const menuButton of menuButtons) {
-  menuButton.addEventListener('click', () => {
-    const newSubmenu = menuButton.querySelector('p').textContent
-    console.log({newSubmenu, currentSubmenu})
-    
-    // Active current button
-    menuButtons.forEach((elem) => elem.classList.remove('active'))
-    menuButton.classList.add('active')
 
-    if (newSubmenu === currentSubmenu) {
-      // Hide menu
-      submenuElem.classList.remove('active')
-      currentSubmenu = ""
-      menuButton.classList.remove('active')
-
-      // Update display
-      setTimeout(() => {
-        submenuElem.style.display = 'none'
-      }, 300)
-
-    } else {
-
-      // Update display
-      submenuElem.style.display = 'block'
-
-      // alculate submenu top
-      const menuHeight = menuElem.offsetHeight
-      submenuElem.style.top = `${menuHeight}px`
-
-      setTimeout(() => {
-        // Show menu
-        submenuElem.classList.add('active')
-        currentSubmenu = newSubmenu
+function ClickButton(e) {
+  const menuButton = e.currentTarget
+  const newSubmenu = menuButton.querySelector('p').textContent
   
+  // Active current button
+  const menuButtonsElems = document.querySelectorAll('.menu > li')
+  menuButtonsElems.forEach((elem) => elem.classList.remove('active'))
+  menuButton.classList.add('active')
 
-      }, 100)
-    }
-  })
+  // Check in the menu button is already active
+  if (newSubmenu === currentSubmenu) {
+    // Hide menu
+    submenuElem.classList.remove('active')
+    currentSubmenu = ""
+    menuButton.classList.remove('active')
+    submenuElem.style.opacity = 0
+
+    // Update display
+    setTimeout(() => {
+      submenuElem.style.display = 'none'
+    }, 300)
+
+
+  // Click in other menu button
+  } else {
+
+    // Update display
+    submenuElem.style.display = 'block'
+
+    // Hide submenu
+    submenuElem.style.opacity = 0
+
+    // Render submenu buttons 
+    setTimeout(() => {
+      const menuButtonData = menusButtonsData.find((elem) => elem.title === newSubmenu)
+
+      let submenuItems = `<ul class="submenu">`
+      for (const submenuItem of menuButtonData.submenu) {
+        const submenuItemText = `
+        <li>
+            <div class="content">
+                <a href="${submenuItem.link}">
+                  <i class="fa ${submenuItem.icon}"></i>
+                  <p>${submenuItem.title}</p>
+                </a>
+            </div>
+        </li>`
+        submenuItems += submenuItemText
+      }
+      submenuItems += `</ul>`
+      submenuElem.innerHTML = submenuItems
+    }, 250)
+
+    // Show submenu again
+    setTimeout(() => {
+      submenuElem.style.opacity = 1
+    }, 300)
+
+    // calculate submenu top
+    const menuHeight = menuElem.offsetHeight
+    submenuElem.style.top = `${menuHeight}px`
+
+    setTimeout(() => {
+      // Show menu
+      submenuElem.classList.add('active')
+      currentSubmenu = newSubmenu
+    }, 100)
+  }
+}
+
+// Render menu buttons
+for (const menuButonData of menusButtonsData) {
+
+  const menuButtonElem = `
+  <li class="${menuButonData.class}" onclick="ClickButton(event)">
+      <div class="content">
+          <i class="fa ${menuButonData.icon}"></i>
+          <p>${menuButonData.title}</p>
+          <div class="description">
+              <p>${menuButonData.description}</p>
+          </div>
+      </div>
+  </li>`
+  menuElem.innerHTML += menuButtonElem  
 }
