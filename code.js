9370c3847c7e82507fe8090a310862cc5089330d
daridev <darideveloper@gmail.com>
2024-07-16 09:04:59 -0600

    feat (buttons/submenu): layout & responsive

--- code.js
+++ code.js
@@ -18,3 +18,48 @@ var splide = new Splide('.splide', {
   padding: '10%',
 })
 splide.mount()
+
+// Open submenu
+let currentSubmenu = ""
+const submenuElem = document.querySelector('.submenu-wrapper')
+const menuButtons = document.querySelectorAll('.menu > li')
+const menuElem = document.querySelector('.menu')
+for (const menuButton of menuButtons) {
+  menuButton.addEventListener('click', () => {
+    const newSubmenu = menuButton.querySelector('p').textContent
+    console.log({newSubmenu, currentSubmenu})
+    
+    // Active current button
+    menuButtons.forEach((elem) => elem.classList.remove('active'))
+    menuButton.classList.add('active')
+
+    if (newSubmenu === currentSubmenu) {
+      // Hide menu
+      submenuElem.classList.remove('active')
+      currentSubmenu = ""
+      menuButton.classList.remove('active')
+
+      // Update display
+      setTimeout(() => {
+        submenuElem.style.display = 'none'
+      }, 300)
+
+    } else {
+
+      // Update display
+      submenuElem.style.display = 'block'
+
+      // alculate submenu top
+      const menuHeight = menuElem.offsetHeight
+      submenuElem.style.top = `${menuHeight}px`
+
+      setTimeout(() => {
+        // Show menu
+        submenuElem.classList.add('active')
+        currentSubmenu = newSubmenu
+  
+
+      }, 100)
+    }
+  })
+}
